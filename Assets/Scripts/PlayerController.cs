9d31d3419db31fa8f59b28c8b54436b0c8938cd4
Munchtoast <dextersydney2001@gmail.com>
2023-02-17 13:22:45 -0500

    refactor(input): Move all key inputs to helper functions
    The current Update() function is bloated with a lot of input checks. I moved all of this into separate helper functions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,49 +11,8 @@ public class PlayerController : MonoBehaviour
     private bool grounded = false;
     private SpriteRenderer spriteRenderer;
 
-    // Start is called before the first frame update
-    void Start()
+    private void KeyAttack()
     {
-        playerRb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Moves player left and right and flips sprite depending on left or right arrow pressed
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left * Time.deltaTime * playerSpeed; 
-            spriteRenderer.flipX = true;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.right * Time.deltaTime * playerSpeed;
-            spriteRenderer.flipX = false;
-        }
-
-        //Adds jump ability and plays jump animation
-        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
-        {
-            grounded = false;
-            playerRb.velocity = new Vector2(0, jumpForce);
-            animator.SetBool("jump", true);
-        }
-
-        //Adds crouch animation
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            animator.SetBool("crouch", true);
-        }
-
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            animator.SetBool("crouch", false);
-        }
-
-
         //Adds attack one animation and checks if space bar is pressed 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -64,6 +23,69 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("attack", false);
         }
+    }
+
+    private void KeyMove()
+    {
+        //Moves player left and right and flips sprite depending on left or right arrow pressed
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime * playerSpeed;
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime * playerSpeed;
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void KeyJump()
+    {
+        //Adds jump ability and plays jump animation
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            grounded = false;
+            playerRb.velocity = new Vector2(0, jumpForce);
+            animator.SetBool("jump", true);
+        }
+    }
+
+    private void KeyCrouch()
+    {
+        //Adds crouch animation
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetBool("crouch", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            animator.SetBool("crouch", false);
+        }
+    }
+
+    private void CheckInput()
+    {
+        KeyMove();
+        KeyJump();
+        KeyCrouch();
+        KeyAttack();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks all Inputs from the player.
+        CheckInput();
 
         //Plays run animation depending on movement of player
         animator.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
