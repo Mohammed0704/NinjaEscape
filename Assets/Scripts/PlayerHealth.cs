eb4b8532c3004669d7c0dafe02423b4c9c9ab5eb
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-03-19 17:36:42 -0400

    Pushing Health Bar UI changes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int health = 3;
     [SerializeField] GameObject gameOver;
+    public Slider healthBar;
 
     // Update is called once per frame
     void Update()
@@ -16,13 +18,50 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+        healthBar.maxValue = health;
+        
+    }
+
     public void deductHealth()
     {
         health--;
+        healthBar.value = health;
     }
 
     public void incrementHealth()
     {
         health++;
+        healthBar.value = health;
     }
 }
+
+
+/*
+public class PlayerHealth : MonoBehaviour
+{
+    public static int health = 5;
+    public Slider healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+        healthBar.maxValue = health;
+        
+    }
+
+    //Checks for collision with spikes
+    private void OnCollisionEnter2D (Collision2D collision){
+        if (collision.gameObject.CompareTag("Player")){
+            health--;
+            healthBar.value = health;
+        }
+    }
+
+    //Check for collision with ghost
+
+    //Check for collision with samurai
+}
+*/
