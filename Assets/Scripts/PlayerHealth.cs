155130428260894313b92c7d7ec1cc0578696321
Munchtoast <dextersydney2001@gmail.com>
2023-03-21 20:04:29 -0400

    BUG(classifier and update): Added public and removed Update()
    If you do not put "public" as a classification, other files cannot access the attribute. This needed to be added.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,37 +5,13 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public static int health = 5;
-   
-    //public GamePlay gameplay;
+    [SerializeField] public int health = 5;
+    [SerializeField] public GameObject gameOver;
+
     public AudioClip playerDeathClip;
     private AudioSource playerAudioSource;
 
     public Slider healthBar;
-
-    // Update is called once per frame
-    
-    /*
-    void Update()
-    {
-        if (health == 0)
-        {   
-            
-            playerAudioSource.PlayOneShot(playerDeathClip);
-            StartCoroutine(WaitAndCallGameOver());
-            /*
-            gameOver.SetActive(true);
-            try
-            {
-                gameOver.GetComponent<GameOverScreen>().GameOver();
-            }
-            catch
-            { }
-            Time.timeScale = 0f;
-            
-        }
-    }
-    */
 
     void Start()
     {
@@ -57,7 +33,7 @@ public class PlayerHealth : MonoBehaviour
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
         Color og_color = spriteRenderer.color;
         spriteRenderer.color = Color.red;
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.1f);
         spriteRenderer.color = og_color;
     }
 
